4bdcd9674b45ed82c071f19e2f075ebb92d59ba3
Alex Ehlke <alex.ehlke@gmail.com>
2024-07-24 17:13:15 -0400

    wip

--- Package.swift
+++ Package.swift
@@ -25,13 +25,13 @@ let package = Package(
                 .copy("PrivacyInfo.xcprivacy")
             ],
             swiftSettings: [
-//                .enableExperimentalFeature("StrictConcurrency")
+                .enableExperimentalFeature("StrictConcurrency")
             ]),
         .testTarget(
             name: "SwiftCloudDriveTests",
             dependencies: ["SwiftCloudDrive"],
             swiftSettings: [
-//                .enableExperimentalFeature("StrictConcurrency")
+                .enableExperimentalFeature("StrictConcurrency")
             ]),
     ],
     swiftLanguageVersions: [.v5, .version("6")]
